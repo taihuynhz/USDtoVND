bf692cc7bd84499d10ad6890d72bc8113b49e7c9
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-01 12:29:17 +0700

    USDtoVND

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDtoVND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float USD;
+            float exchangeRate = 23000f;
+
+            Console.Write("Nhap vao gia tri USD: ");
+            USD = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(USD + " USD = " + USD * exchangeRate + " VND");
+        }
+    }
+}
